92d6b8820f053ce1d278c4837634ee9409d31f61
liandong <liandog@mmears.com>
2019-10-20 23:51:23 +0800

    add ServiceIdConst

---
+++ src/main/java/cn/imustacm/common/consts/ServiceIdConst.java
@@ -0,0 +1,22 @@
+package cn.imustacm.common.consts;
+
+/**
+ * 服务名
+ *
+ * @author liandong
+ * @date 2019/10/20
+ */
+public class ServiceIdConst {
+
+    /**
+     * user服务
+     */
+    public static final String IMUSTACM_USER_SERVICE = "imustacm-user-service";
+
+    /**
+     * problem服务
+     */
+    public static final String IMUSTACM_PROBLEM_SERVICE = "imustacm-problem-service";
+
+
+}
