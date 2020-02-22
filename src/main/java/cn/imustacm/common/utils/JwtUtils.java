7f4395db1475fb48e593b44cbb3f92b057aad64e
liandong <liandog@mmears.com>
2020-02-22 22:49:30 +0800

    迁移JwtUtils到common

---
+++ src/main/java/cn/imustacm/common/utils/JwtUtils.java
@@ -0,0 +1,123 @@
+package cn.imustacm.common.utils;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.common.collect.Maps;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ *
+ * Jwt工具类
+ *
+ * @author: LiAnDong
+ * @since 2020-01-12
+ *
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtUtils {
+
+    private String secret;
+
+    /**
+     * 失效时间ms
+     */
+    private long ttl;
+
+    /**
+     * 生成token
+     *
+     * @param userId
+     * @return
+     */
+    public String createToken(String userId) {
+        Map<String, Object> claims = Maps.newHashMap();
+        return doCreateToken(userId, claims);
+    }
+
+    /**
+     * 生成token
+     *
+     * @param userId
+     * @param claims
+     * @return
+     */
+    public String createToken(String userId, Map<String, Object> claims) {
+        return doCreateToken(userId, claims);
+    }
+
+    private String doCreateToken(String userId, Map<String, Object> claims) {
+        long nowMillis = System.currentTimeMillis();
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userId)
+                .signWith(SignatureAlgorithm.ES256, secret);
+        if (ttl > 0) {
+            jwtBuilder.setExpiration(new Date(nowMillis + ttl));
+        }
+        return jwtBuilder.compact();
+    }
+
+    /**
+     * 获取用户id从token中
+     *
+     * @param token
+     * @return
+     */
+    public String getUserId(String token) {
+        return getClaimFromToken(token).getSubject();
+    }
+
+    /**
+     * 获取payload
+     *
+     * @param token
+     * @return
+     */
+    public Claims getClaimFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(this.secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * 获取token过期时间
+     *
+     * @param token
+     * @return
+     */
+    public Date getExpiration(String token) {
+        return getClaimFromToken(token).getExpiration();
+    }
+
+    public Map<String, Claim> verifyToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(this.secret);
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("imustacm")
+                .build();
+        DecodedJWT jwt = verifier.verify(token);
+        Map<String, Claim> claims = jwt.getClaims();
+        return claims;
+    }
+
+}
+
+
